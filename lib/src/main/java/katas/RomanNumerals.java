868b251c5a73f05a64734ce1ca2aafcd59257973
Pablo Sánchez Alonso <pablo.sanchez.alonso@gmail.com>
2021-07-01 23:42:56 +0100

    Reformatted code

--- lib/src/main/java/katas/RomanNumerals.java
+++ lib/src/main/java/katas/RomanNumerals.java
@@ -1,26 +1,25 @@
 package katas;
 
 public class RomanNumerals {
-    public String convert(int arabic) {
-        return repeatI(arabic)
-            .replace("IIIII", "V")
-            .replace("IIII", "IV")
-            .replace("VV", "X")
-            .replace("VIV", "IX")
-            .replace("XXXXX", "L")
-            .replace("XXXX", "XL")
-            .replace("LXL", "XC")
-            .replace("LL", "C")
-            .replace("CCCCC", "D")
-            .replace("CCCC", "CD")
-            .replace("DD", "M")
-            .replace("DCD", "CM");
-    }
+  public String convert(int arabic) {
+    return repeatI(arabic)
+        .replace("IIIII", "V")
+        .replace("IIII", "IV")
+        .replace("VV", "X")
+        .replace("VIV", "IX")
+        .replace("XXXXX", "L")
+        .replace("XXXX", "XL")
+        .replace("LXL", "XC")
+        .replace("LL", "C")
+        .replace("CCCCC", "D")
+        .replace("CCCC", "CD")
+        .replace("DD", "M")
+        .replace("DCD", "CM");
+  }
 
-    private String repeatI(int times) {
-        StringBuilder builder = new StringBuilder();
-        for(int counter = 0; counter < times; counter++)
-            builder.append("I");
-        return builder.toString();
-    }
+  private String repeatI(int times) {
+    StringBuilder builder = new StringBuilder();
+    for (int counter = 0; counter < times; counter++) builder.append("I");
+    return builder.toString();
+  }
 }
